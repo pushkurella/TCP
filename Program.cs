0aaec71e84a31e711a0f3e7af5a151c27937c29f
pushkurella <35619640+pushkurella@users.noreply.github.com>
2018-01-29 22:39:19 -0500

    code conversion in c#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string sequenceNumber = "00000000";
+            string acknowledgeNumber = "00000000";
+            string flags_data = "51020000";//flags and other info
+            string studentId = "8136590";
+            int sourcePortTemp = int.Parse(studentId.Substring(3, 4));
+            int destinationPortTemp = int.Parse(studentId.Substring(0, 4));
+            string sourcePortHexValue = sourcePortTemp.ToString("X");
+            //int sourcePrt = int.Parse(sourcePortHexValue, System.Globalization.NumberStyles.HexNumber);
+            string destinationPrtHexValue = destinationPortTemp.ToString("X");
+            //int destinationPrt = int.Parse(destinationPrtHexValue, System.Globalization.NumberStyles.HexNumber);
+            sb.Append(sourcePortHexValue);
+            sb.Append(destinationPrtHexValue);
+            sb.Append(sequenceNumber);
+            sb.Append(acknowledgeNumber);
+            sb.Append(flags_data);
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
